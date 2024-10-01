383d85b4fe41c98c7be31d51076ebdd8a250d399
AZHenley <azh321@gmail.com>
2024-09-30 22:41:12 -0400

    Fixed bug.

--- all-code-clipper.zsh
+++ all-code-clipper.zsh
@@ -6,10 +6,10 @@ extensions=("*.c" "*.cpp" "*.py" "*.js" "*.ts" "*.java" "*.go" "*.rb" "*.php" "*
 # Build the find command
 find_command="find . -type f \\("
 for ext in "${extensions[@]}"; do
-    find_command+="-name \"$ext\" -o "
+    find_command+=" -name \"$ext\" -o"
 done
 # Remove the last -o and close the parentheses
-find_command="${find_command::-4}\\)"
+find_command="${find_command::-2} \\)"
 
 # Execute the find command and concatenate files into the clipboard
 eval "$find_command -exec cat {} + | pbcopy"
